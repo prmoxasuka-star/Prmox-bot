876740924f6a6ec70999d93f5f651fc42f8d1a1d
Prmox- Asuka <prmox.asuka@gmail.com>
2025-12-28 20:54:26 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'ᴘʀᴍᴏ✗',
+  author: 'Prmo✗ ᴡᴇʙ',
+  botName: "𝙿𝚁𝙼𝙾✗",
+  botOwner: 'Asuka and Gaïus', // Your name
+  ownerNumber: '94769484004', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.5",
+  updateZipUrl: "https://github.com/prmoxasuka-star/PRMOx-Bot/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
